2a8f28bc03d964e119df3aec24342594521d6532
KevinMasci <kevin-sor@hotmail.com>
2024-02-01 10:58:59 -0300

    inicializar datos de clientes

---
+++ HomeBankingMindHub/Models/DbInitializer.cs
@@ -0,0 +1,18 @@
+﻿namespace HomeBankingMindHub.Models
+{
+    static class DBInitializer
+    {
+        public static void Initialize(HomeBankingContext context)
+        {
+            if(!context.Clients.Any())
+            {
+                var clients = new Client[]
+                {
+                    new Client {Email = "vcoronado@gmail.com", FirstName="Victor", LastName="Coronado", Password="123456"}
+                };
+                context.Clients.AddRange(clients);
+                context.SaveChanges();
+            }
+        }
+    }
+}
