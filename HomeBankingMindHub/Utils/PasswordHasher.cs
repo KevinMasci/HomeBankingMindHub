aa70349416178662286782a8a5b4b9d7ad58dc11
KevinMasci <kevin-sor@hotmail.com>
2024-02-08 12:25:24 -0300

    encriptado de password

---
+++ HomeBankingMindHub/Utils/PasswordHasher.cs
@@ -0,0 +1,17 @@
+﻿namespace HomeBankingMindHub.Utils
+{
+    public class PasswordHasher
+    {
+        public string HashPassword(string password)
+        {
+            // Generar un nuevo hash de contraseña con un salt aleatorio
+            return BCrypt.Net.BCrypt.HashPassword(password, BCrypt.Net.BCrypt.GenerateSalt());
+        }
+
+        public bool VerifyPassword(string password, string hashedPassword)
+        {
+            // Verificar si la contraseña proporcionada coincide con el hash almacenado
+            return BCrypt.Net.BCrypt.Verify(password, hashedPassword);
+        }
+    }
+}
