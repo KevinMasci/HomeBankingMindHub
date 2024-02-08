79652689665020357321dda70ce84099c1e00b9a
KevinMasci <kevin-sor@hotmail.com>
2024-02-08 08:59:27 -0300

    findbyemail method

--- HomeBankingMindHub/Repositories/ClientRepository.cs
+++ HomeBankingMindHub/Repositories/ClientRepository.cs
@@ -34,5 +34,15 @@ namespace HomeBankingMindHub.Repositories
             Create(client);
             SaveChanges();
         }
+
+        public Client FindByEmail(string email)
+        {
+            return FindByCondition(client => client.Email == email)
+                .Include(client => client.Accounts)
+                .Include(client => client.ClientLoans)
+                    .ThenInclude(cl => cl.Loan)
+                .Include(client => client.Cards)
+                .FirstOrDefault();
+        }
     }
 }
