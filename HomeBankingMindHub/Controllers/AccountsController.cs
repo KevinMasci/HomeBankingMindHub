0a82e08b0fbcfe9921cd9bae17b629b43248dbb0
KevinMasci <kevin-sor@hotmail.com>
2024-02-09 11:09:30 -0300

    creacion de cuentas

--- HomeBankingMindHub/Controllers/AccountsController.cs
+++ HomeBankingMindHub/Controllers/AccountsController.cs
@@ -9,10 +9,12 @@ namespace HomeBankingMindHub.Controllers
     public class AccountsController : Controller
     {
         private IAccountRepository _accountRepository;
+        private IClientRepository _clientRepository;
 
-        public AccountsController(IAccountRepository accountRepository)
+        public AccountsController(IAccountRepository accountRepository, IClientRepository clientRepository)
         {
             _accountRepository = accountRepository;
+            _clientRepository = clientRepository;
         }
 
         [HttpGet]
@@ -79,6 +81,97 @@ namespace HomeBankingMindHub.Controllers
                     }).ToList()
                 };
                 return Ok(accountDTO);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
+        }
+
+        [HttpPost("/api/clients/current/accounts")]
+        public IActionResult CreateAccountForCurrentClient()
+        {
+            try
+            {
+                // Obtener el cliente actual
+                var email = User.FindFirst("Client")?.Value;
+                if (string.IsNullOrEmpty(email))
+                {
+                    return Forbid();
+                }
+
+                Client client = _clientRepository.FindByEmail(email);
+
+                if (client == null)
+                {
+                    return Forbid();
+                }
+
+                // Verificar si el cliente ya tiene 3 cuentas registradas
+                if (_accountRepository.GetAccountsByClient(client.Id).Count() >= 3)
+                {
+                    return StatusCode(403, "El cliente ya tiene 3 cuentas registradas, no se puede crear más.");
+                }
+
+                // Crear la cuenta
+                var random = new Random();
+                string accountNumber = "VIN-" + random.Next(100, 99999999).ToString();
+
+                var account = new Account
+                {
+                    ClientId = client.Id,
+                    Number = accountNumber,
+                    Balance = 0,
+                    CreationDate = DateTime.Now,
+                };
+
+                _accountRepository.Save(account);
+                return Ok(account);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
+        }
+
+        [HttpGet("/api/clients/current/accounts")]
+        public IActionResult GetAccountsByCurrentClient()
+        {
+            try
+            {
+                // Obtener el cliente actual
+                var email = User.FindFirst("Client")?.Value;
+                if (string.IsNullOrEmpty(email))
+                {
+                    return Forbid();
+                }
+
+                Client client = _clientRepository.FindByEmail(email);
+
+                if (client == null)
+                {
+                    return Forbid();
+                }
+
+                // Obtener cuentas del cliente
+                var accounts = _accountRepository.GetAccountsByClient(client.Id);
+                var accountsDTO = accounts.Select(account => new AccountDTO
+                {
+                    Id = account.Id,
+                    Number = account.Number,
+                    CreationDate = account.CreationDate,
+                    Balance = account.Balance,
+                    Transactions = account.Transactions.Select(tr => new TransactionDTO
+                    {
+                        Id = tr.Id,
+                        Type = tr.Type.ToString(),
+                        Amount = tr.Amount,
+                        Description = tr.Description,
+                        Date = tr.Date,
+                    }).ToList()
+                }).ToList();
+
+                return Ok(accountsDTO);
             }
             catch (Exception ex)
             {
