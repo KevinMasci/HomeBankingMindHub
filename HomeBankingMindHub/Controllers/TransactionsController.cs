dc4b2c5f8ac98c73b2648d8f57b9bad910f4634b
KevinMasci <kevin-sor@hotmail.com>
2024-02-21 11:25:29 -0300

    refactor validations

--- HomeBankingMindHub/Controllers/TransactionsController.cs
+++ HomeBankingMindHub/Controllers/TransactionsController.cs
@@ -1,6 +1,8 @@
 ﻿using HomeBankingMindHub.DTO;
+using HomeBankingMindHub.Models;
 using HomeBankingMindHub.Repositories;
 using HomeBankingMindHub.Services;
+using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Mvc;
 
 namespace HomeBankingMindHub.Controllers
@@ -9,11 +11,15 @@ namespace HomeBankingMindHub.Controllers
     [ApiController]
     public class TransactionsController : ControllerBase
     {
-        private ITransactionService _transactionService;
+        private readonly ITransactionService _transactionService;
+        private readonly IClientService _clientService;
+        private readonly IAccountService _accountService;
 
-        public TransactionsController(ITransactionService transactionService)
+        public TransactionsController(ITransactionService transactionService, IClientService clientService, IAccountService accountService)
         {
             _transactionService = transactionService;
+            _clientService = clientService;
+            _accountService = accountService;
         }
 
         [HttpPost]
@@ -21,14 +27,59 @@ namespace HomeBankingMindHub.Controllers
         {
             try
             {
+                // Validaciones
                 string email = User.FindFirst("Client") != null ? User.FindFirst("Client").Value : string.Empty;
-
                 if (string.IsNullOrEmpty(email))
                 {
                     return NotFound("Client not found.");
                 }
+                var client = _clientService.FindByEmail(email);
 
-                return _transactionService.MakeTransfer(transferDTO, email);
+                if (client == null)
+                {
+                    return NotFound("The client associated with the current user has not been found.");
+                }
+
+                if (transferDTO.FromAccountNumber == string.Empty || transferDTO.ToAccountNumber == string.Empty)
+                {
+                    return StatusCode(403, "Origin or destination account needed");
+                }
+
+                if (transferDTO.FromAccountNumber == transferDTO.ToAccountNumber)
+                {
+                    return StatusCode(403, "Transfer to same account not allowed.");
+                }
+
+                if (transferDTO.Amount <= 0 || transferDTO.Amount < 0.001)
+                {
+                    return StatusCode(403, "Invalid ammount");
+                }
+
+                if (transferDTO.Description == string.Empty)
+                {
+                    return StatusCode(403, "Description needed.");
+                }
+
+                Account fromAccount = _accountService.GetAccountByNumber(transferDTO.FromAccountNumber);
+                if (fromAccount == null)
+                {
+                    return StatusCode(403, "Origin account does not exists");
+                }
+
+                if (fromAccount.Balance < transferDTO.Amount)
+                {
+                    return StatusCode(403, "insufficient funds");
+                }
+
+                Account toAccount = _accountService.GetAccountByNumber(transferDTO.ToAccountNumber);
+                if (toAccount == null)
+                {
+                    return StatusCode(403, "Destination account does not exists");
+                }
+
+                _transactionService.MakeTransfer(transferDTO, email);
+
+                return Ok("Created");
             }
             catch (Exception ex)
             {
--- HomeBankingMindHub/Services/TransactionService.cs
+++ HomeBankingMindHub/Services/TransactionService.cs
@@ -18,95 +18,41 @@ namespace HomeBankingMindHub.Services
             _transactionRepository = transactionRepository;
         }
 
-        public IActionResult MakeTransfer(TransferDTO transferDTO, string userEmail)
+        public void MakeTransfer(TransferDTO transferDTO)
         {
-            try
+            Account fromAccount = _accountRepository.GetAccountByNumber(transferDTO.FromAccountNumber);
+            Account toAccount = _accountRepository.GetAccountByNumber(transferDTO.ToAccountNumber);
+
+            //comenzamos con la inserrción de las 2 transacciones realizadas
+            //desde toAccount se debe generar un debito por lo tanto lo multiplicamos por -1
+            _transactionRepository.Save(new Transaction
             {
-                Client client = _clientRepository.FindByEmail(userEmail);
+                Type = TransactionType.DEBIT,
+                Amount = transferDTO.Amount * -1,
+                Description = transferDTO.Description + " " + toAccount.Number,
+                AccountId = fromAccount.Id,
+                Date = DateTime.Now,
+            });
 
-                if (client == null)
-                {
-                    return new ObjectResult("No existe el cliente") { StatusCode = 403 };
-                }
-
-                if (transferDTO.FromAccountNumber == string.Empty || transferDTO.ToAccountNumber == string.Empty)
-                {
-                    return new ObjectResult("Cuenta de origen o cuenta de destino no proporcionada.") { StatusCode = 403 };
-                }
-
-                if (transferDTO.FromAccountNumber == transferDTO.ToAccountNumber)
-                {
-                    return new ObjectResult("No se permite la transferencia a la misma cuenta.") { StatusCode = 403};
-                }
-
-                if (transferDTO.Amount <= 0 || transferDTO.Amount < 0.001)
-                {
-                    return new ObjectResult("Monto no valido.") { StatusCode = 403 };
-                }
-
-                if (transferDTO.Description == string.Empty)
-                {
-                    return new ObjectResult("Descripcion no proporcionada.") { StatusCode = 403 };
-                }
-
-                //buscamos las cuentas
-                Account fromAccount = _accountRepository.GetAccountByNumber(transferDTO.FromAccountNumber);
-                if (fromAccount == null)
-                {
-                    return new ObjectResult("Cuenta de origen no existe") { StatusCode = 403};
-                }
-
-                //controlamos el monto
-                if (fromAccount.Balance < transferDTO.Amount)
-                {
-                    return new ObjectResult("Fondos insuficientes") { StatusCode = 403};
-                }
-
-                //buscamos la cuenta de destino
-                Account toAccount = _accountRepository.GetAccountByNumber(transferDTO.ToAccountNumber);
-                if (toAccount == null)
-                {
-                    return new ObjectResult("Cuenta de destino no existe") { StatusCode = 403};
-                }
-
-                //demas logica para guardado
-                //comenzamos con la inserrción de las 2 transacciones realizadas
-                //desde toAccount se debe generar un debito por lo tanto lo multiplicamos por -1
-                _transactionRepository.Save(new Transaction
-                {
-                    Type = TransactionType.DEBIT,
-                    Amount = transferDTO.Amount * -1,
-                    Description = transferDTO.Description + " " + toAccount.Number,
-                    AccountId = fromAccount.Id,
-                    Date = DateTime.Now,
-                });
-
-                //ahora una credito para la cuenta fromAccount
-                _transactionRepository.Save(new Transaction
-                {
-                    Type = TransactionType.CREDIT,
-                    Amount = transferDTO.Amount,
-                    Description = transferDTO.Description + " " + fromAccount.Number,
-                    AccountId = toAccount.Id,
-                    Date = DateTime.Now,
-                });
-
-                //seteamos los valores de las cuentas, a la ccuenta de origen le restamos el monto
-                fromAccount.Balance = fromAccount.Balance - transferDTO.Amount;
-                //actualizamos la cuenta de origen
-                _accountRepository.Save(fromAccount);
-
-                //a la cuenta de destino le sumamos el monto
-                toAccount.Balance = toAccount.Balance + transferDTO.Amount;
-                //actualizamos la cuenta de destino
-                _accountRepository.Save(toAccount);
-
-                return new ObjectResult("Transferencia realizada con exito") { StatusCode = 201 };
-            }
-            catch (Exception ex)
+            //ahora una credito para la cuenta fromAccount
+            _transactionRepository.Save(new Transaction
             {
-                return new ObjectResult($"Error interno del servidor: {ex.Message}") { StatusCode = 500 };
-            }
+                Type = TransactionType.CREDIT,
+                Amount = transferDTO.Amount,
+                Description = transferDTO.Description + " " + fromAccount.Number,
+                AccountId = toAccount.Id,
+                Date = DateTime.Now,
+            });
+
+            //seteamos los valores de las cuentas, a la ccuenta de origen le restamos el monto
+            fromAccount.Balance = fromAccount.Balance - transferDTO.Amount;
+            //actualizamos la cuenta de origen
+            _accountRepository.Save(fromAccount);
+
+            //a la cuenta de destino le sumamos el monto
+            toAccount.Balance = toAccount.Balance + transferDTO.Amount;
+            //actualizamos la cuenta de destino
+            _accountRepository.Save(toAccount);
         }
     }
 }
